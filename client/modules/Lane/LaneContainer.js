9957d3db2c6eead5e0b5b71b066554cd7ed56f76
Maciej Judka <maciej.judka@gmail.com>
2018-03-11 23:21:45 +0100

    Update Kanban.js

--- client/modules/Lane/LaneContainer.js
+++ client/modules/Lane/LaneContainer.js
@@ -1,22 +1,24 @@
 import { connect } from 'react-redux';
 import Lane from './Lane';
-import * as laneActions from './LaneActions';
 import { createNoteRequest } from '../Note/NoteActions';
-
-// const mapStateToProps = (state, ownProps) => {
-// 	return {
-// 		laneNotes: ownProps.lane.notes.map(noteId => state.notes.find(note => note.id === noteId))
-// 	};
-// };
+import { deleteLane, updateLane, editLane } from './LaneActions';
 
 const mapStateToProps = (state, ownProps) => ({
 	laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId])
 });
 
 const mapDispatchToProps = {
-	...laneActions,
+	editLane,
+	deleteLane,
+	updateLane,
 	addNote: createNoteRequest,
 };
+ 
+ 
+// const mapDispatchToProps = {
+// 	...laneActions,
+// 	addNote: createNoteRequest,
+// };
 
 export default connect(
 	mapStateToProps,
